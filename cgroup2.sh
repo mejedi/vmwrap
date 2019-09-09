955f7396fdb392f4fc273e36106c622a86080e92
Nick Zavaritsky <mejedi@gmail.com>
2019-09-09 13:34:03 +0000

    Create cgroup2.sh

---
+++ cgroup2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+. ./init.sh
+
+# cgroup2
+mount -t cgroup2 -o nsdelegate cgroup /sys/fs/cgroup
+echo "+cpu +memory +pids" > /sys/fs/cgroup/cgroup.subtree_control
+mkdir /sys/fs/cgroup/delegated
+chown $vmwrap_uid:$vmwrap_gid \
+    /sys/fs/cgroup/delegated \
+    /sys/fs/cgroup/delegated/cgroup.subtree_control \
+    /sys/fs/cgroup/delegated/cgroup.procs \
+    /sys/fs/cgroup/delegated/cgroup.threads
+echo $vmwrap_pid > /sys/fs/cgroup/delegated/cgroup.procs
